8a4eb8d02f2e2ac3eac3fab403c3a95cafedc22f
Arthur Oliveira <arthurnathanoliveiraleite@outlook.com.br>
2025-09-25 23:47:56 -0300

    Refactor Q4LinkedList:

--- A1/Q4.h
+++ A1/Q4.h
@@ -13,37 +13,6 @@ private:
 	Node *head;
 	int listSize = 0;
 
-public:
-	Q4LinkedList()
-	{
-		head = nullptr;
-		listSize = 0;
-	}
-
-	~Q4LinkedList()
-	{
-		Node *current = head;
-		while (current != nullptr)
-		{
-			Node *next = current->next;
-			delete current;
-			current = next;
-		}
-	}
-
-	void Menu()
-	{
-		int oprt;
-		int val;
-		do{
-			system("cls");
-			std::cout << "INFORME A OPERACAO: \n1 - INSERIR\n2 - REMOVER\n3 - VERIFICAR\n4 - MOSTRAR\nOPERACAO: ";
-			std::cin >> oprt;
-		}while(oprt != 5);
-	}
-
-private:
-
 	void insert(int value)
 	{
 		if (head == nullptr)
@@ -71,69 +40,23 @@ private:
 		listSize++;
 	}
 
-	void remove(int target)
-	{
-		remover(head, target);
-	}
-
-	void remover(Node *start, int value)
-	{
-		if (start == nullptr && listSize == 0)
-			std::cout << "Lista vazia!" << std::endl;
-
-		else if (head->data == value)
-		{
-			Node *remove = head;
-			head = head->next;
-			reIndex(remove->next);
-			delete remove;
-		}
-		else if (start->next->data == value)
-		{
-			Node *remove = start->next;
-			start->next = start->next->next;
-			reIndex(remove);
-			delete remove;
-		}
-		else
-		{
-			remover(start->next, value);
-		}
-
-		listSize--;
-	}
-
 	void display()
 	{
-		displayer(head);
+		displaying(head);
 	}
-	
-	void displayer(Node *start)
+
+	void displaying(Node *start)
 	{
 		if (start != nullptr)
 		{
 			std::cout << "Elemento no indice " << start->index << ": " << start->data << ",  ";
-			displayer(start->next);
+			displaying(start->next);
 		}
 		else if (head == nullptr)
 		{
 			std::cout << "Lista vazia!" << std::endl;
 		}
 	}
-
-	void isOnList(int target)
-	{
-		verify(head, target);
-	}
-
-	
-	void verify(Node *start, int value)
-	{
-		if (start->data == value)
-		std::cout << "Elemento " << value << " encontrado no indice: " << start->index << std::endl;
-		else
-		verify(start->next, value);
-	};
 
 	void reIndex(Node *start)
 	{
@@ -145,5 +68,34 @@ private:
 			current = current->next;
 			i++;
 		}
+	}
+
+public:
+	Q4LinkedList()
+	{
+		head = nullptr;
+		listSize = 0;
+	}
+
+	~Q4LinkedList()
+	{
+		Node *current = head;
+		while (current != nullptr)
+		{
+			Node *next = current->next;
+			delete current;
+			current = next;
+		}
+	}
+
+	void Menu()
+	{
+		int oprt;
+		do
+		{
+			system("cls");
+			std::cout << "INFORME A OPERACAO: \n1 - INSERIR\n2 - REMOVER\n3 - VERIFICAR\n4 - MOSTRAR\n5 - ENCERRAR O PROGRAMA\n\nOPERACAO: ";
+			std::cin >> oprt;
+		} while (oprt != 5);
 	}
 };
